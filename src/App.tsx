50f75effaf83ac401ea9a24b2b8a1428fe460bf2
lzooeun <elinzoo0919@gmail.com>
2026-02-03 21:16:16 -0500

    Initial project structure and shared state in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,56 @@
 import puzzle from "./puzzle.ts";
 import { useState } from "react"
+import Scoreboard from './Scoreboard.tsx'
+import Controls from './Controls.tsx'
 
 export default function App() {
-    return <h1>Starter</h1>;
+    // Shared state for words found by the user
+    const [foundWords, setFoundWords] = useState<string[]>([]); 
+    // Shared state for the current text in the input field
+    const [currentInput, setCurrentInput] = useState(""); 
+
+    // Handler for updating input
+    function handleInputChange(next: string): void {
+        setCurrentInput(next);
+    }
+
+    // Handler for clearing the input
+    function handleResetInput(): void {
+        setCurrentInput("");
+    }
+
+    // Handler for sumitting a word
+    function handleSubmitWord(word: string): void {
+        const normalizedWord = word.toLowerCase().trim();
+
+        if (!foundWords.includes(normalizedWord)) {
+            setFoundWords(foundWords.concat([normalizedWord]));
+        }
+
+        setCurrentInput("")
+    }
+
+
+
+    return (
+        <>
+            <div className="controls-section">
+                <Controls
+                    puzzle={puzzle}
+                    currentInput={currentInput}
+                    foundWords={foundWords}
+                    onInputChange={handleInputChange}
+                    onResetInput={handleResetInput}
+                    onSubmitWord={handleSubmitWord}
+                />
+            </div>
+            <div className="scoreboard-section">
+                <Scoreboard
+                    foundWords={foundWords}
+                    maxScore={puzzle.maxScore}
+                />
+            </div>
+        </>
+    );
 }
 
-function Controls() {
-
-}
-
-function Scoreboard() {
-    
-}
